01f383fc41f875af8be19ea9b43a9b7c6be02323
aissaelouafi <aissaelouafi1994@gmail.com>
2017-06-04 17:34:08 +0200

    Add signal power and moto imagery power + bar chart

--- eeg_processing.m
+++
@@ -1,9 +0,0 @@
-% LOAD EEG signals data 
-% fs = 100 Hz
-
-mat = dir('./data/*.mat');
-for K = 1 : length(mat)
-  thisfile = mat(K).name;
-  %disp(strcat('./data/',thisfile));
-  thisdata = load(strcat('./data/',thisfile));
-end
